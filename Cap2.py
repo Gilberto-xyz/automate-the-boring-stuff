d44546ceacce739df455d9b5e0198a89dfb2bd31
Gilberto NM <gilberto.nava.marcos@outlook.com>
2022-04-23 18:21:43 -0500

    add bugs

--- Cap2.py
+++ Cap2.py
@@ -13,15 +13,19 @@ print('Intenta adivinar el numero. El rango es de 1 a 10')
 for intento in range(3):
     print('Intento', intento + 1)
     numero_usuario = int(input())
-    if numero_usuario == numero_secreto:
-        print('Felicidades, adivinaste el numero')
-        break
+    
     if numero_usuario < numero_secreto:
         print('El numero es mayor')
     if numero_usuario > numero_secreto:
         print('El numero es menor')
+    
+    if numero_usuario == numero_secreto:
+        print('Felicidades, adivinaste el numero')
+        break
+    
     if intento == 2:
         print('Lo siento, no adivinaste el numero')
         print('El numero era', numero_secreto)
-    else:
+        break
+    else: 
         print('Intenta de nuevo')
