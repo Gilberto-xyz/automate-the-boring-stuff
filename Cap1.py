53308e7444ee80476b8b406eb5ecf09cf7909262
Gilberto NM <gilberto.nava.marcos@outlook.com>
2022-04-23 16:27:13 -0500

    Nice to meet you

---
+++ Cap1.py
@@ -0,0 +1,9 @@
+# Este simple programa solo esta interesado en conocerte
+
+print('Cual es tu nombre?')
+nombre = input()
+print(f'Encantado de conocerte {nombre}')
+print(f'Lo largo de tu nombre es {len(nombre)}')
+print('¿Cuantos años tienes?')
+edad = input()
+print(f'Usted tendra {str(int(edad) + 1)} años en el año que viene')
