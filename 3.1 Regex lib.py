8d216d4d7ecda673c97dc5316aac319683355ee8
Gilberto NM <gilberto.nava.marcos@outlook.com>
2022-05-03 00:20:10 -0500

    better names

--- 3.1 Regex lib.py
+++ 3.1 Regex lib.py
@@ -1,7 +1,7 @@
 import re # Libreria para expresiones regulares (RegEx) (10/10)
 
 mensaje = 'Hola, mi nombre es Juan y mi numero es 415-555-1212, cualquier duda que tenga tambien puede enviarme un mensaje a: 415-555-9999'
-
+print(mensaje)
 numero_telefonico_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # Creamos una expresion regular
 
 coincidencias = numero_telefonico_regex.findall(mensaje) # Buscamos los numeros telefonicos en el mensaje
